c9c934a57c53127bb2ecce48eb891fbaa1aa6901
carlosgomes1 <48812516+carlosgomes1@users.noreply.github.com>
2020-03-18 04:18:09 -0300

    Função para gerar mosquito randômico OK

--- game.js
+++ game.js
@@ -1,8 +1,35 @@
+// Ajustando tamanho da tela jogável
 var width, height
 
 function ajustarTamanhoJogo() {
-    width = window.innerWidth, height = window.innerHeight
-    console.log(width, height)
+    width = window.innerWidth
+    height = window.innerHeight
 }
 
 ajustarTamanhoJogo()
+
+function gerarMosquitoRandom() {
+
+    // Gerando posição aleatória para a mosca
+    var random1 = Math.random()
+    var random2 = Math.random()
+
+    var posX = Math.floor(random1 * width) - 90
+    var posY = Math.floor(random2 * height) - 90
+
+    posX = posX < 0 ? posX = 0 : posX = posX
+    posY = posY < 0 ? posY = 0 : posY = posY
+
+    // Criando o elemento HTML com DOM
+
+    var mosquito = document.createElement('img')
+    mosquito.src = 'imagens/mosca.png'
+    mosquito.className = 'mosquito1'
+    mosquito.style.position = 'absolute'
+    mosquito.style.top = posY + 'px'
+    mosquito.style.left = posX + 'px'
+
+    document.body.appendChild(mosquito)
+}
+
+gerarMosquitoRandom()
